5576102f331d0a8417afd6d541b112ae0ccb1d7b
Carlos Requena López <crequena@ulb.ac.be>
2015-03-11 18:52:55 +0100

    printUinter fixed

--- Uinter.cpp
+++ Uinter.cpp
@@ -25,46 +25,56 @@ class Uinter {
   };
   _Inter* _tete;		// pointer to the first interval
 public:
-  Uinter(): _tete(new _Inter(1, 3)) {}; // first interval to test
-  void reunion(int bi, int bs);
+  Uinter(): _tete(new _Inter(3, 7)) {}; // first interval to test
+  void reunion(int new_bi, int new_bs);
   void printUinter();
   bool contient(int nb);
-  _Inter* gettete() {return _tete;};
 };
 
 
 // Uinter member functions
 
-// void reunion(int new_bi, int new_bs) {
-//   void();0;
-// }
+void Uinter::reunion(int new_bi, int new_bs) {
+  // if boundary in common: modify instance of _Inter
+  if (contient(new_bi) and contient(new_bs)) {
+    std::cout << "scam interval!" << std::endl;
+  } else if (not contient(new_bi) and not contient(new_bs)) {
+    std::cout << "new interval!!" << std::endl;
+    _tete -> setnext(new _Inter(new_bi, new_bs));
+  }
+  // if nothing in common: create new instance
+}
 
 
 void Uinter::printUinter() {
   std::cout << "-------------Uinter information---------------" << std::endl;
   std::cout << "Location in memory: " << this << std::endl;
-  std::cout << "Interval(s): " << "[" << _tete -> getbi() << ", " << _tete -> getbs() << "]" << std::endl;
+  std::cout << "Interval(s): ";
+  _Inter* pointer = _tete;
+  do {
+    std::cout << "[" << pointer -> getbi() << ", " << pointer -> getbs() << "] ";
+    pointer = pointer -> getnext();
+  } while (pointer != nullptr);
+  std::cout << std::endl;
   std::cout << "----------------------------------------------" << std::endl;
 }
 
 
 bool Uinter::contient(int nb) {
   _Inter* pointer = _tete;
-  std::cout << "The number to check is: " << nb << std::endl;
 
   bool go = true;
   bool res = false;
   // if number is found, exit loop before
   while (go) {
     if (nb >= pointer -> getbi() and nb <= pointer -> getbs()) {
-      std::cout << "The number IS in the interval" << std::endl;
+      std::cout << "The number " << nb << " IS in the interval" << std::endl;
       go = false;
       res = true;
     } else if (pointer -> getnext() != nullptr) {
-      std::cout << "Going to next interval" << std::endl;
       pointer = pointer -> getnext();
     } else {
-      std::cout << "The number IS NOT in the interval" << std::endl;
+      std::cout << "The number " << nb << " IS NOT in the interval" << std::endl;
       go = false;
     }
   }
@@ -74,7 +84,11 @@ bool Uinter::contient(int nb) {
 
 int main() {
   Uinter interval;
-  interval.contient(5);
+
+  interval.contient(8);
   interval.printUinter();
+  interval.reunion(1, 2);
+  interval.printUinter();
+  interval.contient(19);
   return 0;
 }
