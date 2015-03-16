13aba954676e1d8302aa6613c9a013105148bdd4
Carlos Requena Lopez <crequena@ulb.ac.be>
2015-03-16 15:01:26 +0100

    16/3 changes

--- Uinter.cpp
+++ Uinter.cpp
@@ -15,26 +15,49 @@ class Uinter {
     int _bi, _bs;		// lower and upper bound
     _Inter *_next;		// pointer to next interval
   public:
-    _Inter(int a, int b, _Inter* next=nullptr): _bi(a), _bs(b), _next(next){};
+    _Inter(int a, int b, _Inter* next=nullptr);
     void setbi(int a) {_bi = a;};
     void setbs(int b) {_bs = b;};
     void setnext(_Inter* next) {_next = next;};
     int getbi() const {return _bi;};
     int getbs() const {return _bs;};
     _Inter* getnext() const {return _next;};
+    void checkBoundaries(int new_bi, int new_bs);
   };
   _Inter* _tete;		// pointer to the first interval
 public:
-  Uinter(): _tete(new _Inter(3, 7)) {}; // first interval to test
+  Uinter(): _tete(new _Inter(3, 7, new _Inter(21, 20))) {}; // first interval to test
   void reunion(int new_bi, int new_bs);
   void printUinter();
   bool contient(int nb);
 };
 
+// _Inter constructor
+
+Uinter::_Inter::_Inter(int a, int b, _Inter* next) {
+  std::cout << "Inter being constructed" << std::endl;
+  checkBoundaries(a, b);
+  _bi = a;
+  _bs = b;
+  _next = next;
+}
+
+// _Inter member functions
+
+void Uinter::_Inter::checkBoundaries(int new_bi, int new_bs) {
+  if (new_bs < new_bi) {
+    std::cout << "Not a valid interval" << std::endl;
+    std::exit(0);
+  }
+
+}
+
+// Uinter constructor
 
 // Uinter member functions
 
 void Uinter::reunion(int new_bi, int new_bs) {
+  checkBoundaries(new_bi, new_bs);
   // if boundary in common: modify instance of _Inter
   if (contient(new_bi) and contient(new_bs)) {
     std::cout << "scam interval!" << std::endl;
@@ -48,8 +71,8 @@ void Uinter::reunion(int new_bi, int new_bs) {
 
 void Uinter::printUinter() {
   std::cout << "-------------Uinter information---------------" << std::endl;
-  std::cout << "Location in memory: " << this << std::endl;
-  std::cout << "Interval(s): ";
+  std::cout << "Location in memory: " << *_tete << std::endl;
+  std::cout << "Interval(s)       : ";
   _Inter* pointer = _tete;
   do {
     std::cout << "[" << pointer -> getbi() << ", " << pointer -> getbs() << "] ";
@@ -85,10 +108,7 @@ bool Uinter::contient(int nb) {
 int main() {
   Uinter interval;
 
-  interval.contient(8);
+  interval.contient(15);
   interval.printUinter();
-  interval.reunion(1, 2);
-  interval.printUinter();
-  interval.contient(19);
   return 0;
 }
