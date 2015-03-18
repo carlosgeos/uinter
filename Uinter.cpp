6b4385ff1bd0102cf5f2c018385f60006826d6b1
Carlos Requena Lopez <crequena@ulb.ac.be>
2015-03-19 00:20:19 +0100

    hard coded works more or less

--- Uinter.cpp
+++ Uinter.cpp
@@ -26,7 +26,7 @@ class Uinter {
   };
   _Inter* _tete;		// pointer to the first interval
 public:
-  Uinter(): _tete(new _Inter(3, 7, new _Inter(20, 40, new _Inter(43, 46, new _Inter(50, 60))))) {}; // first interval to test
+  Uinter(): _tete() {}; // first interval(s) to test
   void reunion(int new_bi, int new_bs);
   void reunion(int new_bi, int new_bs, _Inter* pointer);
   void set_interval_bs(int new_bs, _Inter* pointer);
@@ -37,7 +37,7 @@ public:
 // _Inter constructor
 
 Uinter::_Inter::_Inter(int a, int b, _Inter* next) {
-  //std::cout << "Inter being constructed" << std::endl;
+  // check for valid boundaries before constructing the object
   checkBoundaries(a, b);
   _bi = a;
   _bs = b;
@@ -52,7 +52,6 @@ void Uinter::_Inter::checkBoundaries(int new_bi, int new_bs) {
       "Not a valid interval. Exiting" << std::endl;
     std::exit(0);
   }
-
 }
 
 // Uinter member functions
@@ -63,37 +62,40 @@ void::Uinter::reunion(int new_bi, int new_bs) {
 
   // reason: a non-static data member cannot be a default argument for
   // a method
+
   reunion(new_bi, new_bs, _tete);
 }
 
 void Uinter::reunion(int new_bi, int new_bs, _Inter* pointer) {
-  // checkBoundaries needs to be done now!! Otherwise reunion code doesnt work
-  std::cout << "reunion with: " << new_bi << " and " << new_bs << std::endl;
+  pointer -> checkBoundaries(new_bi, new_bs);
+  std::cout << "Applying reunion with interval: [" << new_bi
+	    << ", " << new_bs << "]" << std::endl;
 
-  int bi = pointer -> getbi();
-  int bs = pointer -> getbs();
-
-  if (bi < new_bi and new_bi < bs) {
+  if (pointer == nullptr) {
+    _tete = new _Inter(new_bi, new_bs);
+  } else if (pointer -> getbi() < new_bi and new_bi < pointer -> getbs()) {
     set_interval_bs(new_bs, pointer);
-  } else if (new_bi < bi and new_bs < bi) {
+  } else if (new_bi < pointer -> getbi() and new_bs < pointer -> getbi()) {
     _Inter* next_inter = pointer;
-    pointer = new _Inter(new_bi, new_bs, next_inter);
-  } else if (new_bi < bi) {
-    // careful with this condition, since it can be true for a lot of intervals
+    _tete = new _Inter(new_bi, new_bs, next_inter);
+  } else if (new_bi < pointer -> getbi() and new_bs >= pointer -> getbi()) {
     pointer -> setbi(new_bi);
     set_interval_bs(new_bs, pointer);
+  } else if (pointer -> getnext() == nullptr) {
+    pointer -> setnext(new _Inter(new_bi, new_bs));
   } else {
-    // take a step forward in the list
+    // if it does not match any pattern, take a step forward in the
+    // list
     reunion(new_bi, new_bs, pointer -> getnext());
   }
 }
 
 void Uinter::set_interval_bs(int new_bs, _Inter* pointer) {
-
-  int bs = pointer -> getbs();
+  // sets the upper bound for the given interval and adjusts the next
+  // pointer accordingly
   bool bs_inside = contient(new_bs);
 
-  if (bs_inside and new_bs > bs) {
+  if (bs_inside and new_bs > pointer -> getbs()) {
     do {
       pointer -> setbs(pointer -> getnext() -> getbs());
       pointer -> setnext(pointer -> getnext() -> getnext());
@@ -108,22 +110,27 @@ void Uinter::set_interval_bs(int new_bs, _Inter* pointer) {
 }
 
 
-
-
 void Uinter::printUinter() {
   std::cout << "-------------Uinter information---------------" << std::endl;
   std::cout << "Location in memory: " << _tete << std::endl;
+  std::cout << "Empty interval?   : ";
+  if (_tete == nullptr) {
+    std::cout << "Yes" << std::endl;
+  } else {
+    std::cout << "No" << std::endl;
+  }
   std::cout << "Interval(s)       : ";
   _Inter* pointer = _tete;
-  do {
+  while (pointer != nullptr) {
     std::cout << "[" << pointer -> getbi() << ", " << pointer -> getbs() << "] ";
     pointer = pointer -> getnext();
-  } while (pointer != nullptr);
+  }
   std::cout << std::endl;
   std::cout << "----------------------------------------------" << std::endl;
 }
 
 bool Uinter::contient(int nb) {
+  // iterate over the list, checking if nb is in one of the intervals
   _Inter* pointer = _tete;
 
   bool go = true;
@@ -131,13 +138,11 @@ bool Uinter::contient(int nb) {
   // if number is found, exit loop before
   while (go) {
     if (nb >= pointer -> getbi() and nb <= pointer -> getbs()) {
-      //std::cout << "The number " << nb << " IS in the interval" << std::endl;
       go = false;
       res = true;
     } else if (pointer -> getnext() != nullptr) {
       pointer = pointer -> getnext();
     } else {
-      //std::cout << "The number " << nb << " IS NOT in the interval" << std::endl;
       go = false;
     }
   }
@@ -146,12 +151,70 @@ bool Uinter::contient(int nb) {
 
 //--------------------- Main --------------------------
 int main() {
-  Uinter interval;
+  Uinter a, b, c, d, e, f,g;
+  a.reunion(5,10);
+  a.reunion(6,15);
+  a.reunion(-5,0);
+  a.reunion(100,300);
+  a.reunion(1,1);
+  a.printUinter();
 
-  std::cout << "BEFORE:" << std::endl;
-  interval.printUinter();
-  interval.reunion(-12, 1);
-  std::cout << "AFTER" << std::endl;
-  interval.printUinter();
+  b.reunion(0,0);
+  b.reunion(6,9);
+  b.reunion(-35,-22);
+  b.reunion(2,6);
+  b.reunion(4,50);
+  b.reunion(4,8);
+  b.reunion(2,4);
+  b.reunion(15,17);
+  b.reunion(23,30);
+  b.reunion(-2,16);
+  b.printUinter();
+
+  c.reunion(1,3);
+  c.reunion(5,9);
+  c.reunion(0,0);
+  c.reunion(-1,-1);
+  c.reunion(6,10);
+  c.reunion(3,4);
+  c.printUinter();
+
+  d.reunion(30,564);
+  d.reunion(20,654);
+  d.reunion(10,1237879);
+  d.reunion(-5,0);
+  d.reunion(3,12);
+  d.reunion(4567,123657);
+  d.printUinter();
+
+  e.reunion(-5,0);
+  e.reunion(-6,9);
+  e.reunion(-6,13);
+  e.reunion(-6,45);
+  e.reunion(-6,0);
+  e.printUinter();
+
+  f.reunion(1,1);
+  f.reunion(1,1);
+  f.reunion(1,1);
+  f.reunion(2,2);
+  f.reunion(2,2);
+  f.printUinter();
+
+  g.reunion(0,2);
+  g.reunion(4,8);
+  g.reunion(0,5);
+  g.reunion(100,4000);
+  g.printUinter();
+
   return 0;
+
+
+  // Uinter interval;
+  // std::cout << "Given interval:" << std::endl;
+  // interval.printUinter();
+  // interval.reunion(4, 5);
+  // std::cout << "After modification" << std::endl;
+  // interval.printUinter();
+  // return 0;
 }
