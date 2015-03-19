09911d1b0210e601955fd6b8b6eefcf58db6f46a
Carlos Requena Lopez <crequena@ulb.ac.be>
2015-03-19 16:29:43 +0100

    working commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Interval union C++
 
 Compile with cpp executable
+
+Generate PDF with tex file
--- Uinter.cpp
+++ Uinter.cpp
@@ -25,7 +25,7 @@ class Uinter {
   };
   _Inter* _tete;		// pointer to the first interval
 public:
-  Uinter(): _tete() {}; // first interval(s) to test
+  Uinter(): _tete() {};
   void reunion(int new_bi, int new_bs);
   void reunion(int new_bi, int new_bs, _Inter* pointer);
   void set_interval_bs(int new_bs, _Inter* pointer);
@@ -35,12 +35,14 @@ public:
 
 // =============== _Inter constructor ===============
 
-Uinter::_Inter::_Inter(int a, int b, _Inter* next) {
-  // check for valid boundaries before constructing the object
+Uinter::_Inter::_Inter(int a, int b, _Inter* next) : _bi(a),
+						     _bs(b),
+						     _next(next)
+{
+  /* Constructor with initialization list. Also checks for valid
+     boundaries
+  */
   checkBoundaries(a, b);
-  _bi = a;
-  _bs = b;
-  _next = next;
 }
 
 // =============== _Inter member functions ===============
@@ -56,22 +58,39 @@ void Uinter::_Inter::checkBoundaries(int new_bi, int new_bs) {
 // =============== Uinter member functions ===============
 
 void::Uinter::reunion(int new_bi, int new_bs) {
-  /* this overload has access to the _tete attribute. it is executed
-     when reunion is called with only 2 parameters
+  /* This overload has access to the _tete attribute. It is executed
+     when reunion is called with only 2 parameters (user).
 
-     reason: a non-static data member cannot be a default argument for
-     a method
+     Note: a non-static data member cannot be a default argument for a
+     method
   */
-
+  std::cout << "Applying reunion with interval: [" << new_bi
+	    << ", " << new_bs << "]" << std::endl;
   reunion(new_bi, new_bs, _tete);
 }
 
 void Uinter::reunion(int new_bi, int new_bs, _Inter* pointer) {
-  /* asdf
-     asdf
+  /* Possible patterns (match entries in if statements):
+
+     - The interval is empty
+
+     - The new lower bound is lower than the actual lower bound
+         - The upper bound as well -> create new _Inter below
+	 - Otherwise -> adjust lower bound and upper bound
+
+     - The new lower bound is contained in the actual interval
+         - The upper bound as well -> do nothing
+	 - Otherwise -> simply set the upper bound
+
+     - If reached the end, means interval is bigger and new _Inter
+       is created at the end
+
+     - The new interval is contained between the actual interval
+       and the next -> insert new _Inter there
+
+     - None of these -> iterate through the list
   */
-  std::cout << "Applying reunion with interval: [" << new_bi
-	    << ", " << new_bs << "]" << std::endl;
+
   pointer -> checkBoundaries(new_bi, new_bs);
 
   if (pointer == nullptr) {
@@ -84,7 +103,8 @@ void Uinter::reunion(int new_bi, int new_bs, _Inter* pointer) {
       pointer -> setbi(new_bi);
       set_interval_bs(new_bs, pointer);
     }
-  } else if (new_bi >= pointer -> getbi() and new_bi <= pointer -> getbs()) {
+  } else if (new_bi >= pointer -> getbi() and
+	     new_bi <= pointer -> getbs()) {
     if (new_bs <= pointer -> getbs()) {
       ;
     } else {
@@ -92,59 +112,69 @@ void Uinter::reunion(int new_bi, int new_bs, _Inter* pointer) {
     }
   } else if (pointer -> getnext() == nullptr) {
     pointer -> setnext(new _Inter(new_bi, new_bs));
-  } else if (new_bi < pointer -> getnext() -> getbi() and new_bs < pointer -> getnext() -> getbi()) {
+  } else if (new_bi < pointer -> getnext() -> getbi() and
+	     new_bs < pointer -> getnext() -> getbi()) {
     _Inter* next_inter = pointer -> getnext();
     pointer -> setnext(new _Inter(new_bi, new_bs, next_inter));
-
   } else {
-    // if it does not match any pattern, take a step forward in the
-    // list
     reunion(new_bi, new_bs, pointer -> getnext());
   }
 }
 
 void Uinter::set_interval_bs(int new_bs, _Inter* pointer) {
-  /* sets the upper bound for the given interval and adjusts the next
-     pointer accordingly
+  /* Sets the upper bound for the given interval and adjusts the next
+     pointer accordingly. It also deletes the unused intervals.
   */
 
   bool bs_inside = contient(new_bs);
+  _Inter* jumped;
 
   if (bs_inside and new_bs > pointer -> getbs()) {
     do {
-      pointer -> setbs(pointer -> getnext() -> getbs());
-      pointer -> setnext(pointer -> getnext() -> getnext());
+      jumped = pointer -> getnext();
+      pointer -> setbs(jumped -> getbs());
+      pointer -> setnext(jumped -> getnext());
+      delete jumped;
     } while (new_bs > pointer -> getbs());
   } else if (not bs_inside) {
     pointer -> setbs(new_bs);
-    while (pointer -> getnext() != nullptr and pointer -> getnext() -> getbi() < new_bs) {
-      pointer -> setnext(pointer -> getnext() -> getnext());
+    while (pointer -> getnext() != nullptr and
+	   pointer -> getnext() -> getbi() < new_bs) {
+      jumped = pointer -> getnext();
+      pointer -> setnext(jumped -> getnext());
+      delete jumped;
     }
   }
 }
 
-
 void Uinter::printUinter() {
-  std::cout << "-------------Uinter information---------------" << std::endl;
-  std::cout << "Location in memory: " << _tete << std::endl;
-  std::cout << "Empty interval?   : ";
+  std::cout << "|------------Uinter information-------------" << std::endl;
+  std::cout << "| Empty interval?    : ";
   if (_tete == nullptr) {
     std::cout << "Yes" << std::endl;
   } else {
     std::cout << "No" << std::endl;
   }
-  std::cout << "Interval(s)       : ";
+  std::cout << "| Interval(s)        : ";
   _Inter* pointer = _tete;
+  _Inter* last = pointer;
   while (pointer != nullptr) {
-    std::cout << "[" << pointer -> getbi() << ", " << pointer -> getbs() << "] ";
+    std::cout << "[" << pointer -> getbi() << ", "
+	      << pointer -> getbs() << "] ";
+    last = pointer;
     pointer = pointer -> getnext();
   }
   std::cout << std::endl;
-  std::cout << "----------------------------------------------" << std::endl;
+  std::cout << "| Location in memory : " << _tete
+	    << " to " << last << std::endl;
+  std::cout << "|-------------------------------------------"
+	    << std::endl << std::endl;
 }
 
 bool Uinter::contient(int nb) {
-  // iterate over the list, checking if nb is in one of the intervals
+  /* Iterate over the list, checking if nb is in one of the
+     intervals. If it is, return true, false otherwise.
+  */
   _Inter* pointer = _tete;
 
   bool go = true;		// if number is found, exit loop
@@ -165,6 +195,7 @@ bool Uinter::contient(int nb) {
 
 int main() {
   Uinter interval;
+  interval.printUinter();
 
   int bis[] = {5, 10, 22, 12, 0, 10, 7, 7, 3, 100, 140, -1, -100};
   int bss[] = {8, 15, 30, 23, 0, 25, 7, 9, 5, 150, 170,  0, 1000};
